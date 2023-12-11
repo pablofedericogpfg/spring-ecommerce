565fc197a866b5a5c94935784f101c2af4bbed8a
Pablo Federico Gonzalez <pablofedericog.pfg@gmail.com>
2023-12-10 21:31:11 -0300

    Implementar las funcionalidades de cargar imagen y mostrar imagen para el administrador

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -21,79 +21,93 @@ import com.curso.ecommerce.service.UploadFileService;
 
 @Controller
 @RequestMapping("/productos")
-public class ProductoController  {
-	
+public class ProductoController {
+
 	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
-	
+
 	@Autowired
 	private ProductoService productoService;
-	
+
 	@Autowired
 	private UploadFileService upload;
-	
+
 	@GetMapping("")
 	public String show(Model model) {
 		model.addAttribute("productos", productoService.findAll());
 		return "productos/show";
 	}
+
 	@GetMapping("/create")
 	public String create() {
 		return "productos/create";
 	}
-	
+
 	@PostMapping("/save")
 	public String save(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
-		LOGGER.info("Este es el objeto Producto{}",producto);
-		Usuarios u=new Usuarios(1,"","","","","","","");
+		LOGGER.info("Este es el objeto Producto{}", producto);
+		Usuarios u = new Usuarios(1, "", "", "", "", "", "", "");
 		producto.setUsuario(u);
-		//imagen
-		if (producto.getId()==null) {// cuando se crea un producto siempre el id vendra null
-			String nombreImagen=upload.saveImage(file);
+		// imagen
+		if (producto.getId() == null) {// cuando se crea un producto siempre el id vendra null
+			String nombreImagen = upload.saveImage(file);
 			producto.setImagen(nombreImagen);
 		} else {
-			if (file.isEmpty()) { // cuando editamos producto pero no cambiamos la imagen
-				Producto p=new Producto();
-				p=productoService.get(producto.getId()).get();
-				producto.setImagen(p.getImagen());
-				
-			} else {
-				
-				String nombreImagen=upload.saveImage(file);
-				producto.setImagen(nombreImagen);
-			
-			}
+
 		}
-		
+
 		productoService.save(producto);
-		
+
 		return "redirect:/productos";
-		
+
 	}
-	
+
 	@GetMapping("/edit/{id}")
 	public String edit(@PathVariable Integer id, Model model) {
-		Producto producto= new Producto();
-		Optional<Producto> optionalProducto=productoService.get(id);
-		producto= optionalProducto.get();
-		
-		LOGGER.info("Producto buscado: {}",producto);
+		Producto producto = new Producto();
+		Optional<Producto> optionalProducto = productoService.get(id);
+		producto = optionalProducto.get();
+
+		LOGGER.info("Producto buscado: {}", producto);
 		model.addAttribute("producto", producto);
-		
+
 		return "productos/edit";
 	}
-	
+
 	@PostMapping("/update")
-	public String update(Producto producto) {
-		LOGGER.info("Este es el objeto Producto Actualizar{}",producto);
-		productoService.save(producto);
-	
+	public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
+		Producto p= new Producto();
+		p=productoService.get(producto.getId()).get();
+		
+		if (file.isEmpty()) { // editamos el producto pero no cambiamos la imagem
+			
+			producto.setImagen(p.getImagen());
+		}else {// cuando se edita tbn la imagen			
+			//eliminar cuando no sea la imagen por defecto
+			if (!p.getImagen().equals("default.jpg")) {
+				upload.deleteImage(p.getImagen());
+			}
+			String nombreImagen= upload.saveImage(file);
+			producto.setImagen(nombreImagen);
+		}
+		LOGGER.info("Este es el objeto Producto Actualizar{}", producto);
+		producto.setUsuario(p.getUsuario());
+		productoService.update(producto);		
 		return "redirect:/productos";
-	}
 	
+	}
+
 	@GetMapping("/delete/{id}")
 	public String delete(@PathVariable Integer id) {
-	productoService.delete(id);
-	return "redirect:/productos";
+		Producto p = new Producto();
+		p=productoService.get(id).get();
+		
+		//eliminar cuando no sea la imagen por defecto
+		if (!p.getImagen().equals("default.jpg")) {
+			upload.deleteImage(p.getImagen());
+		}
+		
+		productoService.delete(id);
+		return "redirect:/productos";
 	}
-	
+
 }
--- src/main/java/com/curso/ecommerce/service/UploadFileService.java
+++ src/main/java/com/curso/ecommerce/service/UploadFileService.java
@@ -6,11 +6,16 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.curso.ecommerce.controller.ProductoController;
+
 @Service
 public class UploadFileService {
+	private final Logger LOGGER = LoggerFactory.getLogger(UploadFileService.class);
 	private String folder="images//";
 	public String saveImage(MultipartFile file) throws IOException {
 		if (!file.isEmpty()) {
@@ -26,7 +31,8 @@ public class UploadFileService {
 	}
 	
 	public void deleteImage(String nombre) {
-		String ruta="//images";
+		String ruta="images//";
+		LOGGER.info("Nomber del archivo a borrar{}", ruta+nombre);
 		File file=new File(ruta+nombre);
 		file.delete();
 		
